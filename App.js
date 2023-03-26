9e6dd1a3fbb6d25f7a8b2673f68c4488884ea4a0
PedroHMH01 <rosabranca1070@gmail.com>
2023-03-25 21:22:19 -0300

    Andamento no estudo
    Corrigi alguns bugs que tive nas primeiras aulas e dei andamento imprimindo os itens que serão vendidos e colocando estilo neles

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Servicos from './src/telas/servicos';
+
+export default function App() {
+     return (     <Servicos/>
+     )
+      
+  
+};
+
