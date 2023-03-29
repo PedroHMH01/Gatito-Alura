d03cf84ab949394b85cd1ba55116417031355ca3
PedroHMH01 <rosabranca1070@gmail.com>
2023-03-29 13:22:01 -0300

    Avanço nas aulas da Alura
    Foi colocada a quantidade e preço de cada serviço prestado dentro do app.
    Criando um TextImput colocamos a quantidade de produtos que desejamos colocar no carrinho. nas próximas aulas vamos melhorar essa função e fazer a adição para o carrinho.

--- src/telas/servicos/Item/index.js
+++ src/telas/servicos/Item/index.js
@@ -1,11 +1,29 @@
-import React from "react";
-import {Text, View} from 'react-native';
+import React, {useState} from "react";
+import {Button, Text, TextInput, View} from 'react-native';
+import CampoInteiro from "../../../componentes/campointeiro";
 import estilos from "./estilos";
 
 export default function Item({nome, preco, descricao}){
-    return <View style={estilos.informacao}>
+    const [quantidade, setQuantidade] = useState(1)
+    return <>
+    <View style={estilos.informacao}>
         <Text style={estilos.nome}>{ nome}</Text>
         <Text style={estilos.descricao}>{ descricao}</Text>
         <Text style={estilos.preco}>{ preco}</Text>
     </View>
+    <View style={estilos.carrinho}>
+        <View>
+            <View style={estilos.valor}>
+                <Text style={estilos.descricao}>quantidade:</Text>
+                <CampoInteiro valor={quantidade} acao={setQuantidade}/>
+            </View>
+            <View style={estilos.valor}>
+                <Text style={estilos.descricao}>Preço:</Text>
+                <Text style={estilos.preco}>0</Text>
+            </View>
+        </View>
+        <Button title="adicionar" />
+    </View>
+<View style={estilos.divisor}/>
+    </>
 }
